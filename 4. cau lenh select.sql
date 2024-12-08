f8b5f494e7314223569a4f9268a271276ba4b9d3
Huy Doan <doanvanhuydhdtu@gmail.com>
2024-12-08 21:52:50 +0700

    updated

---
+++ 4. cau lenh select.sql
@@ -0,0 +1,26 @@
+﻿-- Viết câu lệnh SQL lấy ra tên tất cả các sản phẩm
+SELECT [ProductName]
+FROM [dbo].[Products];
+
+-- Viết câu lệnh SQL lấy ra tên sản phẩm, giá bán trên mỗi đơn vị, số lượng sản phẩm trên đơn vị
+SELECT [ProductName], [UnitPrice], [QuantityPerUnit]
+FROM [dbo].[Products]
+
+-- Viết câu lệnh SQL lấy ra tên công ty của khách hàng và quốc gia của khách hàng đó
+SELECT [CompanyName], [Country]
+FROM [dbo].[Customers];
+
+SELECT CompanyName, Country
+FROM dbo.Customers;
+
+-- Viết câu lệnh SQL lấy ra tên công ty và số điện thoại của tất cả nhà cung cấp hàng
+SELECT CompanyName, Phone
+FROM dbo.Suppliers;
+
+-- Lấy tất cả dữ liệu từ bảng Products
+SELECT *
+FROM [dbo].[Products];
+
+-- Lấy tất cả dữ liệu từ bảng Customers
+SELECT *
+FROM [dbo].Customers;
---
+++ 5. cau lenh select distinct.sql
@@ -0,0 +1,24 @@
+﻿/*
+	DICTINCT -> Lấy dữ liệu riêng biệt, ko trùng lặp
+*/
+
+-- Viết câu lệnh SQL lấy ra tên các quốc gia (Country) khác nhau từ bảng khách hàng - Customers
+SELECT DISTINCT [Country]
+FROM [dbo].Customers;
+
+-- Viết câu lệnh SQL lấy ra tên các mã số bưu điện (PostalCode) khác nhau từ bảng Nhà cung cấp - Suppliers
+SELECT DISTINCT [PostalCode]
+FROM [dbo].[Suppliers];
+
+-- Viết câu lệnh SQL lấy ra các dữ liệu khác nhau về họ của nhân viên (LastName) 
+-- và cách gọi danh hiệu lịch sự (TitleOfCourtesy) của nhân viên từ bảng Employees
+SELECT DISTINCT [LastName], [TitleOfCourtesy]
+FROM [dbo].[Employees];
+
+SELECT DISTINCT [TitleOfCourtesy]
+FROM [dbo].[Employees];
+
+SELECT DISTINCT [ShipVia]
+FROM [dbo].[Orders];
+
+
