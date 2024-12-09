edb5e1a22af997147059bb093f4ad87cde58655f
Huy Doan <doanvanhuydhdtu@gmail.com>
2024-12-09 23:04:33 +0700

    updated

---
+++ 10. OrderBy.sql
@@ -0,0 +1,48 @@
+﻿/*
+OrderBy -> sắp xếp kết quả trả về của try vấn
+*/
+-- Bạn hãy liệt kê tất cả các nhà cung cấp theo thứ tự tên đơn vị CompanyName
+-- Từ A-Z
+SELECT *
+FROM [dbo].[Suppliers]
+ORDER BY [CompanyName] ASC; -- ascending
+
+SELECT *
+FROM [dbo].[Suppliers]
+ORDER BY [CompanyName];
+
+-- Bạn hãy liệt kê tất cả các sản phẩm theo thứ tự giá giảm dần.
+SELECT *
+FROM [dbo].[Products]
+ORDER BY [UnitPrice] DESC; -- descending
+
+-- Bạn hãy liệt kê tất cả các nhân viên theo thứ tự họ và tên đệm A-Z.
+-- Không dùng ASC | DESC
+SELECT * 
+FROM [dbo].[Employees]
+ORDER BY [LastName] ASC, [FirstName] ASC;
+
+SELECT * 
+FROM [dbo].[Employees]
+ORDER BY [LastName], [FirstName];
+
+-- Hãy lấy ra một sản phẩm có số lượng bán cao nhất từ bảng [Order Details].
+-- Không được dùng MAX.
+SELECT *
+FROM [dbo].[Order Details]
+ORDER BY [Quantity] DESC;
+
+SELECT TOP 1 *
+FROM [dbo].[Order Details]
+ORDER BY [Quantity] DESC;
+
+-- Hãy liệt kê danh sách các đơn đặt hàng (OrderID) trong bảng Orders theo thứ tự giảm dần của ngày đặt hàng(OrderDate)
+SELECT [OrderID]
+FROM [dbo].[Orders]
+ORDER BY [OrderDate] DESC;
+
+-- Hãy liệt kê tê, đơn giá, số lượng trong kho của tất cả các sản phẩm trong bảng Products, theo thứ tự giảm dần của UnitsInStock
+SELECT [ProductName], [UnitPrice], [UnitsInStock]
+FROM [dbo].[Products]
+ORDER BY [UnitsInStock] DESC;
+
---
+++ 9. count sum avg.sql
@@ -0,0 +1,54 @@
+﻿/*
+ count() -> Đếm số lượng dữ liệu (khác NULL) trong 1 cột, sử dụng COUNT(*) khi muốn đếm số lượng bản ghi
+ sum() -> tính tổng giá trị của 1 cột, nếu bất kỳ giá trị trong cột là NULL, kết quả hàm SUM sẽ là NULL
+ avg() -> tính giá trị trung bình cho cột, nếu tất cả các giá trị trong cột là NULL, kết quả hàm AVG là NULL
+	   -> nếu chỉ có 1 vài giá trị là NULL, AVG sẽ bỏ qua các giá trị NULL và tính trung bình cho các giá trị khác
+*/
+
+-- Hãy đếm số lượng khách hàng có trong bảng (Customers).
+SELECT COUNT(*) [CustomerAmount]
+FROM [dbo].[Customers];
+
+SELECT COUNT([CustomerID]) [CustomerAmount]
+FROM [dbo].[Customers];
+
+-- Tính tổng số tiền vận chuyển (Freight) 
+-- của tất cả các đơn đặt hàng.
+SELECT SUM([Freight]) [SumFreight]
+FROM [dbo].[Orders];
+
+-- Tính trung bình số lượng đặt hàng (Quantity) 
+-- của tất cả các sản phẩm trong bảng [Order Details]
+SELECT AVG([Quantity]) [AvgQuantity]
+FROM [dbo].[Order Details];
+
+-- Đếm số lượng, tính tổng số lượng hàng trong kho 
+-- và trung bình giá của các sản phẩm có trong bảng Product.
+SELECT	COUNT(*) AS [NumberOfProducts], 
+		SUM([UnitsInStock]) AS [TotalUnitsInStock], 
+		AVG([UnitPrice]) AS [AvgUnitPrice]
+FROM [dbo].[Products];
+
+-- Đếm số lượng, tính tổng số lượng hàng tồn kho 
+-- và trung bình giá của các sản phẩm có trong bảng Product.
+SELECT COUNT([QuantityPerUnit]) [NumberOfProducts],
+	   SUM([UnitsInStock] - [UnitsOnOrder]) [TotalUnitsInStock],
+	   AVG([UnitPrice]) [AvgUnitPrice]
+FROM [dbo].[Products];
+
+-- Đếm số lượng đơn hàng từ bảng Orders
+SELECT COUNT(*) [OrderAmount]
+FROM [dbo].[Orders];
+
+SELECT COUNT([OrderID]) [OrderAmount]
+FROM [dbo].[Orders];
+
+-- Từ bảng Order Details hãy tính trung bình cho cột UnitPrice, và tính tổng cho cột Quantity
+SELECT AVG([UnitPrice]) [AvgUnitPrice],
+	   SUM([Quantity]) [SumQuantity]		
+FROM [dbo].[Order Details];
+
+
+
+
+
