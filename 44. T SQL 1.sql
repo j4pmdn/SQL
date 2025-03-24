725fc0a7ca624e77d4c67787b7603adf1ef11a0c
Huy Doan <doanvanhuydhdtu@gmail.com>
2025-03-24 11:31:34 +0700

    TSQL

---
+++ 44. T SQL 1.sql
@@ -0,0 +1,19 @@
+﻿/*
+	T-SQL (Transact-SQL) là cú pháp ngôn ngữ lập trình mở rộng của ngôn ngữ SQL gồm có các tính năng
+	được Sybase và Microsoft bổ sung cho SQL. Chẳng hạn như kiểm sát giao dịch, xử lý lỗi và các biến 
+	được khai báo
+
+
+*/
+
+DECLARE @CustomerName NVARCHAR(50)
+DECLARE @OrderTotal MONEY
+
+-- Gán giá trị cho biến
+SET @CustomerName  = 'John Doe'
+SET @OrderTotal = 100.50
+
+-- Sử dụng biến trong truy vấn
+SELECT *
+FROM Orders
+WHERE CustomerName = @CustomerName;
